47ece35fa7bc4a035bbcfbbb545bf2efa52e5981
Fatma Degirmenci <fatosdegirmenci644@gmilcom>
2025-08-03 10:51:24 +0100

    Level100 is completed

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+  pageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
+function pageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";
+
+  episodeList.forEach((episode) => {
+    const seasonStr = episode.season.toString().padStart(2, "0");
+    const episodeStr = episode.number.toString().padStart(2, "0");
+    const code = `S${seasonStr}E${episodeStr}`;
+
+    const divForEpisode = document.createElement("div");
+    divForEpisode.className = "episode";
+
+    const title = document.createElement("h2");
+    title.textContent = `${episode.name} (${code})`;
+    divForEpisode.appendChild(title);
+
+    const image = document.createElement("img");
+    image.src = episode.image.medium;
+    image.alt = `Image for ${episode.name}`;
+    divForEpisode.appendChild(image);
+
+    const summary = document.createElement("div");
+    summary.innerHTML = episode.summary;
+
+    divForEpisode.appendChild(summary);
+
+    rootElem.appendChild(divForEpisode);
+  });
+
+  const info = document.createElement("p");
+  info.innerHTML =
+    'Data from <a href="https://www.tvmaze.com/" target="_blank" rel="noopener noreferrer">TVMaze.com</a>';
+  rootElem.appendChild(info);
 }
 
 window.onload = setup;
